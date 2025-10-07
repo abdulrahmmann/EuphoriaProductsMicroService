f21ba88b37a49aba3acd554f4697ba7285d486c5
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-07 17:35:47 +0300

    Add Packages, Setup Dependency Injection on Layers, Setup Program.cs File and adding ExceptionHandling Middleware.

---
+++ ProductsMicroService.Application/ModuleDependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProductsMicroService.Application;
+
+public static class ModuleDependencyInjection
+{
+    public static IServiceCollection AddApplicationDependency(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register Mapster
+        services.AddMapster();
+        var config = TypeAdapterConfig.GlobalSettings;
+        // config.Scan(typeof(UserMappingConfig).Assembly);
+        
+        // Register Mediatr
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        // Register Fluent Validation
+        
+        
+        return services;
+    }
+}
